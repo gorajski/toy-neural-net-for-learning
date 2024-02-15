ffbc93e0253228ce9cc8776526e63a7897ff893e
Aaron Sullivan <aasullivan1618@gmail.com>
2024-02-15 14:45:12 -0800

    We have a working neural "net", a single neuron!

---
+++ main.ts
@@ -0,0 +1,38 @@
+import Neuron from './neuron'
+const activationFunction = (n: number) => Math.exp(n) / (1 + Math.exp(n))
+const neuron = new Neuron(activationFunction, [Math.random(), Math.random()], 0)
+const trainingData = [
+    {
+        inputs: [-1, -1],
+        desiredOutput: -1
+    },
+    {
+        inputs: [1, -1],
+        desiredOutput: 1
+    },
+    {
+        inputs: [-1, 1],
+        desiredOutput: 1
+    },
+    {
+        inputs: [1, 1],
+        desiredOutput: 1
+    },
+]
+for (let i = 0; i < 1000000; i++) {
+    trainingData.forEach(data => {
+        const gradientArray = neuron.backprop(data.inputs, data.desiredOutput)
+        // console.log("gradientArray:", gradientArray)
+        neuron.updateWeights(gradientArray)
+    })
+
+    if (i % 1000 === 0) {
+        console.log("iteration:", i)
+        console.log(neuron)
+    }
+}
+
+console.log("### FINAL:")
+trainingData.forEach(data => {
+    console.log(data.inputs, neuron.eval(data.inputs))
+})
--- neuron.ts
+++ neuron.ts
@@ -11,14 +11,45 @@ export default class Neuron {
         this.bias = bias
     }
 
+    /** The value our neuron gives when firing, given these inputs */
     eval(inputs: number[]) {
         // TODO Validate that inputs.length is same as weights.length
-        const weightedSumOfInputs: number = this.weights.reduce(
+        const weightedSumOfInputs = this.weightedSumOfInputs(inputs)
+        return this.actFunc(weightedSumOfInputs + this.bias)
+    }
+
+    weightedSumOfInputs(inputs: number[]): number {
+        return this.weights.reduce(
             (acc: number, weight: number, index: number) => (
                 acc + (weight * inputs[index])
             ),
         0)
+    }
 
-        return this.actFunc(weightedSumOfInputs + this.bias)
+    /** See img/math.jpg for the math of what's going on here */
+    backprop(inputs: number[], desiredOutput: number): number[] {
+        // (dCost/dWeight)[], slope of our function, where we want it to be 0
+        const gradientArray: number[] = []
+
+        // The value our neuron gives when firing, given these inputs
+        const actualOutput = this.eval(inputs)
+        const weightedSum = this.weightedSumOfInputs(inputs)
+        const actualOutputPerWeightedSum = Math.exp(weightedSum) / Math.pow((1 + Math.exp(weightedSum)), 2)
+
+        inputs.forEach(input => {
+            const weightedSumPerWeight = input
+            const costPerActualOutput = 2 * (actualOutput - desiredOutput)
+            const costPerWeight = costPerActualOutput * actualOutputPerWeightedSum * weightedSumPerWeight
+            gradientArray.push(costPerWeight)
+        })
+
+        return gradientArray
+    }
+
+    /** Modifies local neuron weights */
+    updateWeights(gradientArray: number[]) {
+        gradientArray.forEach((gradient, index) => {
+            this.weights[index] -= gradient * 0.1
+        })
     }
 }
