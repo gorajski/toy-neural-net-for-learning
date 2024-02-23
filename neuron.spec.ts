131efb64ce61b64abfef3386322dddf710b947de
Brandon Dees <dees@pm.me>
2024-02-23 16:02:51 -0600

    backprop with bias

--- neuron.spec.ts
+++ neuron.spec.ts
@@ -1,141 +1,207 @@
-import { describe, it, expect } from 'vitest'
-import Neuron from "./neuron"
+import { describe, expect, it } from "vitest";
+import Neuron from "./neuron";
 
-describe("Neuron", function() {
-    describe("constructor", function () {
-        it("accepts a callback for an activation function", function() {
-            const neuron = new Neuron(() => {
-                return 0.7
-            }, [], 4.3)
+describe("Neuron", function () {
+  describe("constructor", function () {
+    it("accepts a callback for an activation function", function () {
+      const neuron = new Neuron(
+        () => {
+          return 0.7;
+        },
+        [],
+        4.3,
+      );
 
-            expect(typeof neuron.actFunc).toBe("function")
-            expect(neuron.actFunc(Math.random())).toBe(0.7)
-        })
+      expect(typeof neuron.actFunc).toBe("function");
+      expect(neuron.actFunc(Math.random())).toBe(0.7);
+    });
 
-        it("accepts an array of float values between 0 and 1 for initial weights", function() {
-            const initialWeights = [1, 0.5, 0]
-            const neuron = new Neuron((n: number) => n, initialWeights, 4.3)
-            expect(neuron.weights).toEqual(initialWeights)
-        })
+    it("accepts an array of float values between 0 and 1 for initial weights", function () {
+      const initialWeights = [1, 0.5, 0];
+      const neuron = new Neuron((n: number) => n, initialWeights, 4.3);
+      expect(neuron.weights).toEqual(initialWeights);
+    });
 
-        it("accepts a float value for bias", function() {
-            const neuron = new Neuron((n: number) => n, [], 4.3)
-            expect(neuron.bias).toEqual(4.3)
-        })
+    it("accepts a float value for bias", function () {
+      const neuron = new Neuron((n: number) => n, [], 4.3);
+      expect(neuron.bias).toEqual(4.3);
+    });
 
-        // defaults for weights and bias if none provided??
-    })
+    // defaults for weights and bias if none provided??
+  });
 
-    describe("#eval", function() {
+  describe("#eval", function () {
+    it("returns value of the bias weight when all inputs are 0, regardless of weights", function () {
+      const inputs: number[] = [0, 0];
+      const weights: number[] = [3.0, 1.1];
+      const bias: number = 52;
 
-        it("returns value of the bias weight when all inputs are 0, regardless of weights", function() {
-            const inputs: number[] = [0, 0]
-            const weights: number[] = [3.0, 1.1]
-            const bias: number = 52
+      const neuron: Neuron = new Neuron(
+        (x: number) => {
+          return x;
+        },
+        weights,
+        bias,
+      );
+      expect(neuron.eval(inputs)).toBe(bias);
+    });
 
-            const neuron: Neuron = new Neuron((x: number) => {return x}, weights, bias)
-            expect(neuron.eval(inputs)).toBe(bias)
-        })
+    it("returns value of the bias when all the weights are 0, regardless of input", function () {
+      const inputs: number[] = [999, -8765];
+      const weights: number[] = [0, 0];
+      const bias: number = 3;
 
-        it("returns value of the bias when all the weights are 0, regardless of input", function() {
-            const inputs: number[] = [999, -8765]
-            const weights: number[] = [0, 0]
-            const bias: number = 3
+      const neuron: Neuron = new Neuron(
+        (x: number) => {
+          return x;
+        },
+        weights,
+        bias,
+      );
+      expect(neuron.eval(inputs)).toBe(bias);
+    });
 
-            const neuron: Neuron = new Neuron((x: number) => {return x}, weights, bias)
-            expect(neuron.eval(inputs)).toBe(bias)
-        })
+    describe("assuming the activation function is the identity function", function () {
+      const identityFunction = (weightedSum: number): number => {
+        return weightedSum;
+      };
 
-        describe("assuming the activation function is the identity function", function() {
-            const identityFunction = (weightedSum: number): number => { return weightedSum }
+      it("produces a linear combination of weights and inputs", function () {
+        const inputs: number[] = [2.0, 3.0, 5.0];
+        const weights: number[] = [7.0, 11.0, 13.0];
+        const bias: number = 17;
 
-            it("produces a linear combination of weights and inputs", function() {
-                const inputs: number[] = [2.0, 3.0, 5.0]
-                const weights: number[] = [7.0, 11.0, 13.0]
-                const bias: number = 17
+        const neuron: Neuron = new Neuron(identityFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(129);
+      });
 
-                const neuron: Neuron = new Neuron(identityFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(129)
-            })
+      it("returns 0.5 when all the inputs are 1.0, and any single weight is 0.5 while the rest are 0 and the bias is 0", function () {
+        const inputs: number[] = [1.0, 1.0, 1.0];
+        const weights: number[] = [0, 0.5, 0];
+        const bias: number = 0;
 
-            it("returns 0.5 when all the inputs are 1.0, and any single weight is 0.5 while the rest are 0 and the bias is 0", function() {
-                const inputs: number[] = [1.0, 1.0, 1.0]
-                const weights: number[] = [0, 0.5, 0]
-                const bias: number = 0
+        const neuron: Neuron = new Neuron(identityFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(0.5);
+      });
 
-                const neuron: Neuron = new Neuron(identityFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(0.5)
-            })
+      it("returns 0.5 when all the weights are 1.0, and any single input is 0.5 while the rest are 0 and the bias is 0", function () {
+        const inputs: number[] = [0, 0.5, 0];
+        const weights: number[] = [1.0, 1.0, 1.0];
+        const bias: number = 0;
 
-            it("returns 0.5 when all the weights are 1.0, and any single input is 0.5 while the rest are 0 and the bias is 0", function() {
-                const inputs: number[] = [0, 0.5, 0]
-                const weights: number[] = [1.0, 1.0, 1.0]
-                const bias: number = 0
+        const neuron: Neuron = new Neuron(identityFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(0.5);
+      });
 
-                const neuron: Neuron = new Neuron(identityFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(0.5)
-            })
+      it("returns sum of all the inputs weighted by the neuron weights plus the bias", function () {
+        const inputs: number[] = [0.25, 6];
+        const weights: number[] = [100, -3];
+        const bias: number = 12;
 
-            it("returns sum of all the inputs weighted by the neuron weights plus the bias", function() {
-                const inputs: number[] = [0.25, 6]
-                const weights: number[] = [100, -3]
-                const bias: number = 12
+        const neuron: Neuron = new Neuron(identityFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(19);
+      });
+    });
 
-                const neuron: Neuron = new Neuron(identityFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(19)
-            })
-        })
+    describe("assuming the activation function is the unit step function", function () {
+      const stepFunction = (weightedSum: number): number => {
+        return weightedSum >= 0 ? 1 : 0;
+      };
 
-        describe("assuming the activation function is the unit step function", function() {
-            const stepFunction = (weightedSum: number): number => {
-                return weightedSum >= 0 ? 1 : 0
-            }
+      it("returns 1.0 when all the inputs and weights are 1.0 and bias is 0.0", function () {
+        const inputs: number[] = [1.0, 1.0, 1.0];
+        const weights: number[] = [1.0, 1.0, 1.0];
+        const bias: number = 0;
 
-            it("returns 1.0 when all the inputs and weights are 1.0 and bias is 0.0", function() {
-                const inputs: number[] = [1.0, 1.0, 1.0]
-                const weights: number[] = [1.0, 1.0, 1.0]
-                const bias: number = 0
+        const neuron: Neuron = new Neuron(stepFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(1.0);
+      });
 
-                const neuron: Neuron = new Neuron(stepFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(1.0)
-            })
+      it("returns 1.0 when all the inputs are 1.0, and any single weight is 0.5 while the rest are 0 and the bias is 0", function () {
+        const inputs: number[] = [1.0, 1.0, 1.0];
+        const weights: number[] = [0, 0.5, 0];
+        const bias: number = 0;
 
-            it("returns 1.0 when all the inputs are 1.0, and any single weight is 0.5 while the rest are 0 and the bias is 0", function() {
-                const inputs: number[] = [1.0, 1.0, 1.0]
-                const weights: number[] = [0, 0.5, 0]
-                const bias: number = 0
+        const neuron: Neuron = new Neuron(stepFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(1.0);
+      });
 
-                const neuron: Neuron = new Neuron(stepFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(1.0)
-            })
+      it("returns 1.0 when all the weights are 1.0, and any single input is 0.5 while the rest are 0 and the bias is 0", function () {
+        const inputs: number[] = [0, 0.5, 0];
+        const weights: number[] = [1.0, 1.0, 1.0];
+        const bias: number = 0;
 
-            it("returns 1.0 when all the weights are 1.0, and any single input is 0.5 while the rest are 0 and the bias is 0", function() {
-                const inputs: number[] = [0, 0.5, 0]
-                const weights: number[] = [1.0, 1.0, 1.0]
-                const bias: number = 0
+        const neuron: Neuron = new Neuron(stepFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(1.0);
+      });
 
-                const neuron: Neuron = new Neuron(stepFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(1.0)
-            })
+      it("returns 1.0 when weighted sum + bias is positive", function () {
+        const inputs: number[] = [0.25, 6];
+        const weights: number[] = [100, -3];
+        const bias: number = 12;
 
-            it("returns 1.0 when weighted sum + bias is positive", function() {
-                const inputs: number[] = [0.25, 6]
-                const weights: number[] = [100, -3]
-                const bias: number = 12
+        const neuron: Neuron = new Neuron(stepFunction, weights, bias);
+        expect(neuron.eval(inputs)).toBe(1.0);
+      });
+    });
 
-                const neuron: Neuron = new Neuron(stepFunction, weights, bias)
-                expect(neuron.eval(inputs)).toBe(1.0)
-            })
-        })
+    describe("When activation func returns zero", function () {
+      it("returns 0 always", function () {
+        const inputs = [Math.random(), Math.random(), Math.random()];
+        const weights = [Math.random(), Math.random(), Math.random()];
+        const bias = Math.random();
+        const neuron: Neuron = new Neuron(() => 0, weights, bias);
+        expect(neuron.eval(inputs)).toBe(0);
+      });
+    });
+  });
 
-        describe("When activation func returns zero", function() {
-            it("returns 0 always", function() {
-                const inputs = [Math.random(), Math.random(), Math.random()]
-                const weights = [Math.random(), Math.random(), Math.random()]
-                const bias = Math.random()
-                const neuron: Neuron = new Neuron(() => 0, weights, bias)
-                expect(neuron.eval(inputs)).toBe(0)
-            })
-        })
-    })
-})
+  describe("#backprop", function () {
+    it("returns an array of the same length as the weights plus one, 0th value is the bias", function () {
+      const neuron: Neuron = new Neuron((n: number) => n, [1, 1, 1], 0);
+      const gradientArray: number[] = neuron.backprop([1, 1, 1], 3);
+      expect(gradientArray.length).toBe(4);
+      expect(gradientArray[0]).toBe(0);
+
+      const neuron2: Neuron = new Neuron((n: number) => n, [1, 1, 1], 0);
+      const gradientArray2: number[] = neuron2.backprop([1, 1, 1], 4);
+      expect(gradientArray2[0]).toBeLessThan(0);
+
+      const neuron3: Neuron = new Neuron((n: number) => n, [1, 1, 1], 0);
+      const gradientArray3: number[] = neuron3.backprop([1, 1, 1], 2);
+      expect(gradientArray3[0]).toBeGreaterThan(0);
+    });
+
+    it("returns an array of numbers", function () {
+      const neuron: Neuron = new Neuron((n: number) => n, [1, 2, 3], 4);
+      const gradientArray: number[] = neuron.backprop([1, 2, 3], 4);
+      gradientArray.forEach((gradient: number) => {
+        expect(typeof gradient).toBe("number");
+      });
+    });
+
+    it("returns an array of numbers that are close to zero when the inputs are 0", function () {
+      const neuron: Neuron = new Neuron((n: number) => n, [1, 2, 3], 4);
+      const gradientArray: number[] = neuron.backprop([0, 0, 0], 4);
+      gradientArray.forEach((gradient: number) => {
+        expect(gradient).toBeCloseTo(0);
+      });
+    });
+
+    it.skip("?? returns an array of numbers that are close to zero when the inputs are 1", function () {
+      const neuron: Neuron = new Neuron((n: number) => n, [1, 2, 3], 4);
+      const gradientArray: number[] = neuron.backprop([1, 1, 1], 4);
+      gradientArray.forEach((gradient: number) => {
+        expect(gradient).toBeCloseTo(0);
+      });
+    });
+
+    it.skip("?? returns an array of numbers that are close to zero when the inputs are 0.5", function () {
+      const neuron: Neuron = new Neuron((n: number) => n, [1, 2, 3], 4);
+      const gradientArray: number[] = neuron.backprop([0.5, 0.5, 0.5], 4);
+      gradientArray.forEach((gradient: number) => {
+        expect(gradient).toBeCloseTo(0);
+      });
+    });
+  });
+});
--- neuron.ts
+++ neuron.ts
@@ -1,55 +1,67 @@
-import { ActFunc } from './types'
+import { ActFunc } from "./types";
 
 export default class Neuron {
-    actFunc: ActFunc
-    weights: number[]
-    bias: number
+  actFunc: ActFunc;
+  weights: number[];
+  bias: number;
 
-    constructor(actFunc: ActFunc, initialWeights: number[], bias: number) {
-        this.actFunc = actFunc
-        this.weights = initialWeights
-        this.bias = bias
-    }
+  constructor(actFunc: ActFunc, initialWeights: number[], bias: number) {
+    this.actFunc = actFunc;
+    this.weights = initialWeights;
+    this.bias = bias;
+  }
 
-    /** The value our neuron gives when firing, given these inputs */
-    eval(inputs: number[]) {
-        // TODO Validate that inputs.length is same as weights.length
-        const weightedSumOfInputs = this.weightedSumOfInputs(inputs)
-        return this.actFunc(weightedSumOfInputs + this.bias)
-    }
+  /** The value our neuron gives when firing, given these inputs */
+  eval(inputs: number[]) {
+    // TODO Validate that inputs.length is same as weights.length
+    const weightedSumOfInputs = this.weightedSumOfInputs(inputs);
+    return this.actFunc(weightedSumOfInputs + this.bias);
+  }
 
-    weightedSumOfInputs(inputs: number[]): number {
-        return this.weights.reduce(
-            (acc: number, weight: number, index: number) => (
-                acc + (weight * inputs[index])
-            ),
-        0)
-    }
+  weightedSumOfInputs(inputs: number[]): number {
+    return this.weights.reduce(
+      (acc: number, weight: number, index: number) => (
+        acc + (weight * inputs[index])
+      ),
+      0,
+    );
+  }
 
-    /** See img/math.jpg for the math of what's going on here */
-    backprop(inputs: number[], desiredOutput: number): number[] {
-        // (dCost/dWeight)[], slope of our function, where we want it to be 0
-        const gradientArray: number[] = []
+  /** See img/math.jpg for the math of what's going on here */
+  backprop(inputs: number[], desiredOutput: number): number[] {
+    // (dCost/dWeight)[], slope of our function, where we want it to be 0
+    const gradientArray: number[] = [];
 
-        // The value our neuron gives when firing, given these inputs
-        const actualOutput = this.eval(inputs)
-        const weightedSum = this.weightedSumOfInputs(inputs)
-        const actualOutputPerWeightedSum = Math.exp(weightedSum) / Math.pow((1 + Math.exp(weightedSum)), 2)
+    const actualOutput = this.eval(inputs);
+    const weightedSum = this.weightedSumOfInputs(inputs);
+    const actualOutputPerWeightedSum = Math.exp(weightedSum) /
+      Math.pow(1 + Math.exp(weightedSum), 2);
+    const costPerActualOutput = 2 * (actualOutput - desiredOutput);
 
-        inputs.forEach(input => {
-            const weightedSumPerWeight = input
-            const costPerActualOutput = 2 * (actualOutput - desiredOutput)
-            const costPerWeight = costPerActualOutput * actualOutputPerWeightedSum * weightedSumPerWeight
-            gradientArray.push(costPerWeight)
-        })
+    const weightedSumPerBias = 1;
+    const costPerBias = costPerActualOutput * actualOutputPerWeightedSum *
+      weightedSumPerBias;
+    gradientArray.push(costPerBias);
 
-        return gradientArray
-    }
+    inputs.forEach((input) => {
+      const weightedSumPerWeight = input;
+      const costPerWeight = costPerActualOutput * actualOutputPerWeightedSum *
+        weightedSumPerWeight;
+      gradientArray.push(costPerWeight);
+    });
 
-    /** Modifies local neuron weights */
-    updateWeights(gradientArray: number[]) {
-        gradientArray.forEach((gradient, index) => {
-            this.weights[index] -= gradient * 0.1
-        })
-    }
+    return gradientArray;
+  }
+
+  /** Modifies local neuron weights */
+  updateWeights(gradientArray: number[]) {
+    gradientArray.forEach((gradient, index) => {
+      this.weights[index] -= gradient * 0.1;
+    });
+  }
+
+  update(inputs: number[], desiredOutput: number) {
+    const gradientArray = this.backprop(inputs, desiredOutput);
+    this.updateWeights(gradientArray);
+  }
 }
