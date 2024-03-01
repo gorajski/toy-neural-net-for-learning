1af9731114244735d586973d9fb1fd743dfc0bf0
Aaron Sullivan <aasullivan1618@gmail.com>
2024-03-01 14:17:09 -0800

    Main now with spacing

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 import Neuron from "./neuron";
+
 const activationFunction = (n: number) => Math.exp(n) / (1 + Math.exp(n));
+
 const neuron = new Neuron(activationFunction, [
   Math.random(),
   Math.random(),
   Math.random(),
 ], -0.1);
+
 const trainingData = [
   {
     inputs: [-1, -1, -1],
@@ -39,6 +42,7 @@ const trainingData = [
     desiredOutput: 1,
   },
 ];
+
 for (let i = 0; i < 1000000; i++) {
   trainingData.forEach((data) => {
     neuron.update(data.inputs, data.desiredOutput);
